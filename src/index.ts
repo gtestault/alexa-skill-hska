a2d68792e7927be722ee3df6ab8cfdc4dd94f7f8
Gabriel <gabriel@testault.com>
2020-11-02 18:01:09 +0100

    initial commit

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-const app = express()
-const PORT = 8000
-app.get("/", (req, res) => res.send("Hello World"))
-
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-})
